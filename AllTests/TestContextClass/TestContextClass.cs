3c71493d7986c078710cd47e50046378f348f754
vit-ganich <vit-ganich@yandex.ru>
2019-03-11 17:24:09 +0300

    updates

---
+++ AllTests/TestContextClass/TestContextClass.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BddFrameworkSpecflowUdemy.AllTests.TestContextClass
+{
+    [TestClass]
+    public class TestContextClass
+    {
+        private TestContext _testContext;
+
+        public TestContext TestContext
+        {
+            get { return _testContext; }
+            set { _testContext = value;  }
+        }
+
+        [TestMethod]
+        public void TestCase1()
+        {
+            Console.WriteLine("Test name : {0}", TestContext.TestName);
+        }
+
+        [TestMethod]
+        public void TestCase2()
+        {
+            Console.WriteLine("Test name : {0}", TestContext.TestName);
+        }
+
+        [TestCleanup]
+        public void AfterTest()
+        {
+            Console.WriteLine("Test result : {0}", TestContext.CurrentTestOutcome);
+        }
+    }
+}
