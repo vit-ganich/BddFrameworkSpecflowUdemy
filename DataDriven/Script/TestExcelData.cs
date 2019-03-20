bd27b7638980f001d63339f46e19e1cad22974f6
vit-ganich <vit-ganich@yandex.ru>
2019-03-20 14:31:33 +0300

    updates

---
+++ DataDriven/Script/TestExcelData.cs
@@ -0,0 +1,38 @@
+﻿using ExcelDataReader;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+using System.Data;
+
+namespace BddFrameworkSpecflowUdemy
+{
+    [TestClass]
+    public class TestExcelData
+    {
+        [TestMethod]
+        public void TestReadExcel()
+        {
+            //FileStream stream = new FileStream(@"E:\Scripts\BddFrameworkSpecflowUdemy\DataDriven\TestData\TestTable.xlsx", FileMode.Open, FileAccess.Read);
+            //IExcelDataReader reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+            //DataTable table = reader.AsDataSet().Tables[0];
+
+            //for (int i = 0; i < table.Rows.Count; i++){
+            //    var col = table.Rows[i];
+            //    for (int j = 0; j < col.ItemArray.Length; j++)
+            //    {
+            //        Console.Write("Data : {0}", col.ItemArray[j]);
+            //    }
+            //    Console.WriteLine(); 
+            //}
+            string excelPath = @"E:\Scripts\BddFrameworkSpecflowUdemy\DataDriven\TestData\TestVariosDataTypes.xlsx";
+            int i = 0;
+            while (i < 3)
+            {
+                Console.WriteLine(ExcelReaderHelper.GetCellData(excelPath, "Test", 0, i));
+                i++;
+            }
+        }
+    }
+}
---
+++ ExcelReader/ExcelReaderHelper.cs
@@ -0,0 +1,52 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+
+namespace BddFrameworkSpecflowUdemy
+{
+    public class ExcelReaderHelper
+    {
+        private static IDictionary<string, IExcelDataReader> _cache;
+        private static FileStream stream;
+        private static IExcelDataReader reader;
+
+        static ExcelReaderHelper()
+        {
+            _cache = new Dictionary<string, IExcelDataReader>();
+        }
+
+        public static object GetCellData(string excelPath, string sheetName, int row, int column)
+        {
+            if (_cache.ContainsKey(sheetName))
+            {
+                reader = _cache[sheetName];
+            }
+            else
+            {
+                stream = new FileStream(excelPath, FileMode.Open, FileAccess.Read);
+                reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                _cache.Add(sheetName, reader);
+            }
+            DataTable table = reader.AsDataSet().Tables[sheetName];
+
+            var result = table.Rows[row][column];
+            return GetData(result.GetType(), result);
+        }
+
+        private static object GetData(Type type, object data)
+        {
+            switch (type.Name)
+            {
+                case "Double":
+                    return Convert.ToDouble(data);
+                case "DateTime":
+                    return Convert.ToDateTime(data);
+                default:
+                    return data.ToString();
+            }
+        }
+    }
+}
