b56b9cf4fe07ba3214a615c696d6ec37ed3a4495
vit-ganich <vit-ganich@yandex.ru>
2019-03-28 12:00:53 +0300

    updates

--- AllTests/FileUploadTest/TestFIleUpload.cs
+++ AllTests/FileUploadTest/TestFIleUpload.cs
@@ -15,19 +15,48 @@ namespace BddFrameworkSpecflowUdemy.AllTests
         [TestMethod]
         public void TestUpload()
         {
+
+            string autoITscriptLocation = @"E:\Scripts\BddFrameworkSpecflowUdemy\AutoIT\UploadFile.exe";
+            string pathToUploadFile = @"E:\Scripts\BddFrameworkSpecflowUdemy\AllTests\FileUploadTest\TestData\FileToUpload.txt";
+
             NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebSiteUrl());
 
             LinkHelper.ClickLink(By.XPath("//*[@title='Contact Us']"));
 
-            ButtonHelper.ClickButton(By.Id("fileUpload"));
+            /**
+             * Firefox Gecko driver cannot click on input element with type “file”
+             * See StackOwerflow: https://stackoverflow.com/questions/52411584/firefox-gecko-driver-cannot-click-on-input-element-with-type-file
+             * 
+             * Firefox (via geckodriver) is behaving correctly here and Chrome is not. 
+             * The W3C WebDriver Specification, in its description of the Element Click 
+             * algorithm in Section 14.1, step 3 states: 
+             * “If the element is an input element in the file upload state1 return error 
+             * with error code invalid argument.”
+             * 
+             * To successfully upload files using WebDriver, 
+             * you should use the sendKeys method with the full path to the file you want to upload.
+             **/
 
-            ProcessStartInfo processInfo = new ProcessStartInfo();
-            processInfo.FileName = @"E:\Scripts\BddFrameworkSpecflowUdemy\AutoIT\UploadFile.exe";
-            processInfo.Arguments = @"E:\Scripts\BddFrameworkSpecflowUdemy\AllTests\FileUploadTest\TestData\FileToUpload.txt";
+            if (ObjectRepository.Config.GetBrowser() == BrowserType.Chrome)
+            {
+                ButtonHelper.ClickButton(By.Id("fileUpload"));
+                ProcessStartInfo processInfo = new ProcessStartInfo()
+                {
+                    FileName = autoITscriptLocation,
+                    Arguments = pathToUploadFile
+                };
 
-            Process process = Process.Start(processInfo);
-            process.WaitForExit();
-            process.Close();
+                using (Process process = Process.Start(processInfo))
+                {
+                    process.WaitForExit();
+                }
+            }
+            else
+            {
+                TextBoxHelper.TypeInTextBox(By.Id("fileUpload"), pathToUploadFile);
+            }
+
+            System.Threading.Thread.Sleep(1000);
         }
     }
 }
